b110a2a6bc03853444718a1b032472b0b9a6c930
Jacob <jacoblodenius@gmail.com>
2022-11-14 13:58:15 +0100

    more nvim conf

---
+++ .config/nvim/lua/jacob/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+treesitter.setup({
+	highlight = { enable = true },
+	indent = { enable = true },
+	autotag = { enable = true },
+	-- Languages
+	-- ADD MORE (https://github.com/nvim-treesitter/nvim-treesitter#supported-languages)
+	ensure_installed = {
+		"json",
+		"javascript",
+		"typescript",
+		"tsx",
+		"yaml",
+		"html",
+		"css",
+		"markdown",
+		"graphql",
+		"bash",
+		"lua",
+		"vim",
+		"dockerfile",
+		"gitignore",
+	},
+	auto_install = true,
+})
