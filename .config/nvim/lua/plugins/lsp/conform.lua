971ef4c6418099e44c8494b5e560a9799607247f
Jacob <jacoblodenius@gmail.com>
2024-04-10 23:01:52 +0200

    check for prettier conf before formatting w prettier

--- .config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/lua/plugins/lsp/conform.lua
@@ -4,6 +4,32 @@ return {
   config = function()
     local conform = require("conform")
 
+    local function find_root_with_markers(starting_path, root_markers)
+      local path = vim.fn.fnamemodify(starting_path, ":p:h")
+      local sep = package.config:sub(1, 1)
+
+      while path and #path > 1 do
+        for _, marker in ipairs(root_markers) do
+          local marker_path = path .. sep .. marker
+          if vim.fn.isdirectory(marker_path) ~= 0 or vim.fn.filereadable(marker_path) ~= 0 then return path end
+        end
+        path = vim.fn.fnamemodify(path, ":h")
+      end
+      return nil
+    end
+
+    local function root_has_file(starting_path, root_markers, target_files)
+      local root_path = find_root_with_markers(starting_path, root_markers)
+      if root_path then
+        local sep = package.config:sub(1, 1)
+        for _, file in ipairs(target_files) do
+          local file_path = root_path .. sep .. file
+          if vim.fn.filereadable(file_path) ~= 0 then return true end
+        end
+      end
+      return false
+    end
+
     -- More settings & formatters
     -- https://github.com/stevearc/conform.nvim
     conform.setup({
@@ -23,6 +49,17 @@ return {
         yaml = { "prettier" },
         lua = { "stylua" },
         python = { "black" },
+      },
+      formatters = {
+        prettier = {
+          condition = function()
+            local root_markers = { ".git", "package.json" }
+            local target_files = { ".prettierrc", ".prettierrc.json" }
+            local starting_path = vim.fn.expand("%:p")
+            return root_has_file(starting_path, root_markers, target_files)
+          end,
+          prepend_args = { "--no-semi", "--single-quote" },
+        },
       },
       format_on_save = {
         lsp_fallback = true,
