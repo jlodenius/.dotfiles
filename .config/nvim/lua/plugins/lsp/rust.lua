0ac6ef45cb3f4699abefbc76cb247a2c8745487f
Jacob <jacoblodenius@gmail.com>
2024-10-20 21:13:20 +0200

    rust setup

--- .config/nvim/lua/plugins/lsp/rust.lua
+++ .config/nvim/lua/plugins/lsp/rust.lua
@@ -1,10 +1,32 @@
 return {
-  "rust-lang/rust.vim",
-  ft = { "rust" },
-  config = function()
-    vim.g.rustfmt_autosave = 1
-    vim.g.rustfmt_emit_files = 1
-    vim.g.rustfmt_fail_silently = 0
-    vim.g.rust_clip_command = "wl-copy"
-  end,
+  {
+    "rust-lang/rust.vim",
+    ft = { "rust" },
+    config = function()
+      vim.g.rustfmt_autosave = 1
+      vim.g.rustfmt_emit_files = 1
+      vim.g.rustfmt_fail_silently = 0
+      vim.g.rust_clip_command = "wl-copy"
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    event = { "BufRead Cargo.toml" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("crates").setup({
+        completion = {
+          cmp = {
+            enabled = true,
+          },
+        },
+      })
+      require("cmp").setup.buffer({
+        sources = { { name = "crates" } },
+      })
+    end,
+  },
 }
