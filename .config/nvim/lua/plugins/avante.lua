acc937b2e8249e2b03957bc78695629e3cb84322
jlodenius <jacoblodenius@gmail.com>
2025-03-10 22:04:11 +0100

    ..

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -12,8 +12,9 @@ return {
     auto_suggestions_provider = "claude",
     claude = {
       endpoint = "https://api.anthropic.com",
-      -- model = "claude-3-5-sonnet-20241022",
-      model = "claude-3-5-haiku-20241022",
+      -- See models:
+      -- https://docs.anthropic.com/en/docs/about-claude/models/all-models
+      model = "claude-3-7-sonnet-20250219",
       temperature = 0,
       max_tokens = 4096,
     },
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -30,6 +30,15 @@ return {
         "python",
       },
       sync_install = false,
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          scope_incremental = "<S-CR>",
+          node_decremental = "<BS>",
+        },
+      },
     })
   end,
 }
