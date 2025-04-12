f1fedba2c2de88b3220813da8d8cf7f01e546780
jlodenius <jacoblodenius@gmail.com>
2025-04-12 14:45:13 +0200

    idk really

--- .config/fish/functions/clean_nvim_swap.fish
+++ .config/fish/functions/clean_nvim_swap.fish
@@ -2,9 +2,14 @@ function clean_nvim_swap --description 'Clean Neovim swap files'
   set -l swap_dir ~/.local/share/nvim/swap
 
   if test -d $swap_dir
-    set -l count (count $swap_dir/*)
-    rm -f $swap_dir/*
-    echo "Cleaned $count swap files from Neovim swap directory"
+    set -l files $swap_dir/*
+    if test (count $files) -gt 0
+      set -l count (count $files)
+      rm -f $files
+      echo "Cleaned $count swap files from Neovim swap directory"
+    else
+      echo "No swap files to clean in $swap_dir"
+    end
   else
     echo "Neovim swap directory not found at $swap_dir"
   end
