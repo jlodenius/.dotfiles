94a90227d60cc636b832dfab5d919bb625d4dccf
jlodenius <jacoblodenius@gmail.com>
2025-11-22 22:40:54 +0100

    test

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,10 +63,10 @@ vim.cmd([[
 ]])
 
 -- workaround for rust analyzer issue
-for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-  local default_diagnostic_handler = vim.lsp.handlers[method]
-  vim.lsp.handlers[method] = function(err, result, context, config)
-    if err ~= nil and err.code == -32802 then return end
-    return default_diagnostic_handler(err, result, context, config)
-  end
-end
+-- for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+--   local default_diagnostic_handler = vim.lsp.handlers[method]
+--   vim.lsp.handlers[method] = function(err, result, context, config)
+--     if err ~= nil and err.code == -32802 then return end
+--     return default_diagnostic_handler(err, result, context, config)
+--   end
+-- end
