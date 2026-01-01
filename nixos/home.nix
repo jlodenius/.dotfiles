14b7c2180f0a052831136dc172819b8f8d934aa3
jacob <jacoblodenius@gmail.com>
2026-01-01 20:17:51 +0100

    Generation 22: 2026-01-01 20:17:51

--- nixos/home.nix
+++ nixos/home.nix
@@ -35,6 +35,36 @@
     };
     interactiveShellInit = ''
       fish_vi_key_bindings
+
+      # Set colors
+      set -g fish_color_autosuggestion 747369
+      set -g fish_color_command 99cc99
+      set -g fish_color_comment ffcc66
+      set -g fish_color_cwd green
+      set -g fish_color_cwd_root red
+      set -g fish_color_end cc99cc
+      set -g fish_color_error f2777a
+      set -g fish_color_escape 66cccc
+      set -g fish_color_history_current --bold
+      set -g fish_color_match 6699cc
+      set -g fish_color_normal normal
+      set -g fish_color_operator 6699cc
+      set -g fish_color_param d3d0c8
+      set -g fish_color_quote ffcc66
+      set -g fish_color_redirection d3d0c8
+      set -g fish_color_search_match white --background=brblack
+      set -g fish_color_selection white --bold --background=brblack
+      set -g fish_color_status red
+      set -g fish_color_user brgreen
+      set -g fish_color_valid_path --underline
+
+      # Pager colors
+      set -g fish_pager_color_description B3A06D
+      set -g fish_pager_color_progress brwhite --background=cyan
+      set -g fish_pager_color_selected_background --background=brblack
+
+      # Disable the greeting
+      set -g fish_greeting ""
     '';
     plugins = [
       {
@@ -71,7 +101,6 @@
     gh
     direnv
     nix-direnv
-    zoxide
     fzf
   ];
 
